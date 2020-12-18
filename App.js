5ee5825eed13b9db35e9c1655bdfb80e6fad06c2
Joeseph McCann <joesephmccann@Joesephs-MacBook-Air.local>
2020-12-17 23:31:29 -0600

    added logic to incrament and decrament count on button press

--- App.js
+++ App.js
@@ -6,13 +6,22 @@ const { useState } = React;
 
 export default function App() {
   const [count, setCount] = useState(0);
+
+  const IncreaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const DecreaseCount = () => {
+    count > 0 ? setCount(count - 1) : null;
+  };
+
   return (
     <View style={styles.container}>
       <Text>Counter App</Text>
       <Text>{count}</Text>
       <View style={{ flexDirection: "row", justifyContent: 'space-between', width: '50%' }}>
-        <Button style={styles.button} title="Increase" />
-        <Button style={styles.button} title="Decrease" />
+        <Button onPress={IncreaseCount} style={styles.button} title="Increase" />
+        <Button onPress={DecreaseCount} style={styles.button} title="Decrease" />
       </View>
     </View >
   );
