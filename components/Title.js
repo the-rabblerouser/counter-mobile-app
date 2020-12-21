23e752e5fdd87f4039cbb4a19275dd0774e207c8
Joeseph McCann <joesephmccann@Joesephs-MacBook-Air.local>
2020-12-21 16:45:40 -0600

    moved title to its own component

---
+++ components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Title = () => {
+    return (
+        <View>
+            <Text>Counter App</Text>
+        </View>
+    );
+};
+
+export default Title;
